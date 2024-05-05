ba0443977e6ef1f6810d2b8fd3739663ce94c72f
simon <simonik1002@mail.ru>
2024-05-05 22:18:11 +0400

    Add Requests table

---
+++ TH.Dal/Entities/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace TH.Dal.Entities;
+
+public class Request
+{
+	public Guid Id { get; set; }
+	public Guid DocumentId { get; set; }
+	public Guid ParentDocumentId { get; set; }
+	public int EndRow { get; set; }
+}
